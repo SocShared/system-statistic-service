8ca7048366fb55b2c7c4fa36281b83ff231e08c9
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-16 03:53:51 +0300

    Фикс авторизации (передача токена)

--- src/main/java/ml/socshared/stat/client/SentryFeignClient.java
+++ src/main/java/ml/socshared/stat/client/SentryFeignClient.java
@@ -4,12 +4,14 @@ import ml.socshared.stat.domain.response.SentryIssueResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "sentry-client", url = "${sentry.url:}")
 public interface SentryFeignClient {
 
     @GetMapping(value = "/api/0/projects/socshared/socshared-service/issues/")
-    SentryIssueResponse[] getIssues(@RequestParam(name = "query", required = false) String query);
+    SentryIssueResponse[] getIssues(@RequestParam(name = "query", required = false) String query,
+                                    @RequestHeader("Authorization") String token);
 
 }
--- src/main/java/ml/socshared/stat/service/impl/SentryServiceImpl.java
+++ src/main/java/ml/socshared/stat/service/impl/SentryServiceImpl.java
@@ -5,6 +5,7 @@ import ml.socshared.stat.client.SentryFeignClient;
 import ml.socshared.stat.domain.response.SentryIssueResponse;
 import ml.socshared.stat.service.SentryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +14,15 @@ public class SentryServiceImpl implements SentryService {
 
     private final SentryFeignClient client;
 
+    @Value("${sentry.api.key}")
+    private String token;
+
+    private String token() {
+        return "Bearer " + token;
+    }
+
     @Override
     public SentryIssueResponse[] getIssues(String query) {
-        return client.getIssues(query);
+        return client.getIssues(query, token());
     }
 }
