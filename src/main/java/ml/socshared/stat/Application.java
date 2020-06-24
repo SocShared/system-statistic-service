9a40151854b05f206320149e374a164198040e70
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-24 20:23:21 +0300

    Добавлен EnableScheduling

--- src/main/java/ml/socshared/stat/Application.java
+++ src/main/java/ml/socshared/stat/Application.java
@@ -6,9 +6,11 @@ package ml.socshared.stat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
